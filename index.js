46c7024957dfd8bdcdbb4bd4842d76fe3ab70955
Logan Dean <lgdean@users.noreply.github.com>
2019-04-13 19:39:01 -0700

    add parcel; serve only the intended files
    1. add a build step to put needed files into dist/
    2. serve from that directory

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const path = require('path')
 const PORT = process.env.PORT || 5000
 
 express()
-  .use(express.static(path.join(__dirname, '')))
+  .use(express.static(path.join(__dirname, 'dist')))
   .get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
